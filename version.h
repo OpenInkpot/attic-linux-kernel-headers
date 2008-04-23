76b8bfb07172479d968e9bb235848c3892287ac6
Mikhail Gusarov <dottedmag@dottedmag.net>
2008-04-24 00:17:56 +0700

    armel support: version bumped to 2.6.18
    armel toolchain requires at least 2.6.14 to compile, so fixing version.h to tell
    the truth is the good idea.

--- version.h
+++ version.h
@@ -1,3 +1,3 @@
-#define UTS_RELEASE "2.6.12"
-#define LINUX_VERSION_CODE 132620
+#define UTS_RELEASE "2.6.18"
+#define LINUX_VERSION_CODE 132626
 #define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
